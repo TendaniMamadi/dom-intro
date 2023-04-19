1bc522d57d59e17201fb12e7c35f8b01a2ea93a5
TendaniMamadi <mamadi.tendani@gmail.com>
2023-04-19 08:17:51 +0200

    modified

--- js/text-bill.js
+++ js/text-bill.js
@@ -12,36 +12,36 @@
 // * display the latest total on the screen
 
 
-let smsTotalElement = document.querySelector(".smsTotalOne")
-let callTotalElement = document.querySelector(".callTotalOne")
+let smsTotalElement2 = document.querySelector(".smsTotalOne")
+let callTotalElement2 = document.querySelector(".callTotalOne")
 let billTotalElement2 = document.querySelector(".totalOne")
 let billTypeText = document.querySelector(".billTypeText");
 
 let addToBtn = document.querySelector(".addToBillBtn")
 
 
-var callTotal = 0;
-var smsTotal = 0;
-var overallBillTotal2 = 0;
 
+var callTotal2 = 0;
+var smsTotal2 = 0;
+var overallBillTotal2 = 0;
 
 function textBill() {
 
     var billTypeVal = billTypeText.value;
 
     if (billTypeVal.toLowerCase() == 'sms') {
-        smsTotal += 0.75;
+        smsTotal2 += 0.75;
 
     } else if (billTypeVal.toLowerCase()  == 'call') {
-        callTotal += 2.75;
+        callTotal2 += 2.75;
     };
 
-    overallBillTotal2 = smsTotal + callTotal;
+    overallBillTotal2 = smsTotal2 + callTotal2;
 
     
     
-    smsTotalElement.innerHTML = smsTotal.toFixed(2);
-    callTotalElement.innerHTML = callTotal.toFixed(2);
+    smsTotalElement2.innerHTML = smsTotal2.toFixed(2);
+    callTotalElement2.innerHTML = callTotal2.toFixed(2);
     
     billTotalElement2.innerHTML = overallBillTotal2.toFixed(2);
     
